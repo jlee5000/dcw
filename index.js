b9aee7c31d96799bcc8665a0430cde7f824c79a2
james lee <james@medio123.com>
2017-10-10 22:46:08 -0700

    added test cases

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 /**
  * documentCreateWrapper
  *
- * @param type the element type, 'div', 'p', 'img' etc.
- * @param attributes array of objects. [{'key':'value'}]
- * @param append append any value. usually document.createTextNode() for text.
+ * @param string - type the element type, 'div', 'p', 'img' etc.
+ * @param object - attributes array of objects. [{'key':'value'}]
+ * @param object - append append any value. usually document.createTextNode() for text.
  *        You can nest this function over and over until you have the desired effect you want.
  *
  * @returns {object}
  */
 module.exports = function(type, attributes, append) {
+
+    if (arguments.length===0) {
+        throw 'no arguments present';
+    }
+
+    if(typeof type !== 'string') {
+        throw 'given dom type is not a string';
+    }
+
     let doc = document.createElement(type);
-    if((attributes !== null && attributes !== undefined) &&
-        attributes.length > 0) {
+
+    if((attributes !== null && attributes !== undefined) && attributes.length > 0) {
         for (let i = 0; i < attributes.length; i++) {
             let keys = Object.keys(attributes[i]);
-            doc.setAttribute(keys[0], attributes[i][keys[0]]);
+            if(keys.length > 1) {
+                for(let g = 0; g < keys.length; g++) {
+                    doc.setAttribute(keys[g], attributes[i][keys[g]]);
+                }
+            } else {
+                doc.setAttribute(keys[0], attributes[i][keys[0]]);
+            }
         }
     }
+
     if(append !== null && append !== undefined && Array.isArray(append)===true) {
         for(let j=0; j<append.length; j++) {
             if(typeof append[j] === 'string') {
@@ -30,5 +46,6 @@ module.exports = function(type, attributes, append) {
     } else if(append !== null && append !== undefined && (typeof append === 'string' || typeof append === 'number')) {
         doc.appendChild(document.createTextNode(append));
     }
+
     return doc;
 };
--- spec.js
+++ spec.js
@@ -1,5 +1,6 @@
 var jsdom = require('mocha-jsdom');
 var expect = require('chai').expect;
+var assert = require('chai').assert;
 
 describe('Document Create Wrapper', function() {
 
@@ -8,6 +9,14 @@ describe('Document Create Wrapper', function() {
     jsdom();
     before(function () {
         dcm = require('./index');
+    });
+
+    it('should should throw exception - no arguments present', function() {
+        assert.throws(function () { dcm() }, 'no arguments present', "Error thrown");
+    });
+
+    it('should should throw exception - given dom type is not a string', function() {
+        assert.throws(function () { dcm({}) }, 'given dom type is not a string', "Error thrown");
     });
 
     it('should should return an <div></div>', function() {
@@ -21,8 +30,13 @@ describe('Document Create Wrapper', function() {
     });
 
     it('should should return an <div class="foo" id="bar">text</div>', function() {
-        div = dcm('div',[{"class":"foo"},{"id":"bar"}], 'text');
+        div = dcm('div',[{"class":"foo", "id":"bar"}], 'text');
         expect(div.outerHTML).to.be.equal('<div class="foo" id="bar">text</div>');
+    });
+
+    it('should should return an <div class="foo" id="bar">text</div>', function() {
+        div = dcm('div',[{"class":"bar"},{"id":"foo"}], 'text');
+        expect(div.outerHTML).to.be.equal('<div class="bar" id="foo">text</div>');
     });
 
 });
